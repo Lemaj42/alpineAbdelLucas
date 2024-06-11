18415050c05d1375b91ca669b5dcd9eb0cd29c45
lemaj42 <abdelmadjidchabane89@gmail.com>
2024-06-11 15:40:33 +0200

    Section Footer terminé

--- src/Pages/Accueil.js
+++ src/Pages/Accueil.js
@@ -1,6 +1,7 @@
 import React from "react";
-import History from "./Histoire";
+import History from "./History";
 import Header from "./header";
+import videoSrc from '../assetes/videos/210611_A110_X_FelipePantone_Main_Edit_45s-1680x951px.mp4';
 
 
 function Accueil() {
@@ -9,11 +10,15 @@ function Accueil() {
             <div className="d-flex justify-content-center">
                 <Header />
             </div>
-
             <History />
-            <div> dimansion </div>
-            <div> Loreojz rgpoi zetoi zaron,g^rkgùpngo t^p gùoe obùeigo lk g </div>
-            <div> fin </div>
+            <section className="video-section">
+                <div className="video-container">
+                    <video autoPlay loop muted>
+                        <source src={videoSrc} type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+            </section>
         </section>
 
     </>
---
+++ src/Pages/Footer.js
@@ -0,0 +1,83 @@
+import React from "react";
+import './CSS/Footer.css';
+import Logo from '../assetes/images/Logo2.jpg'
+import { NavLink } from "react-router-dom";
+
+function Footer() {
+    return (
+        <section>
+            <footer className="BgSection1 bg-body-tertiary text-center">
+                <div className="container p-4">
+
+                    <section className="LogoFooter mb-4">
+                        <h1 className="mb-5">
+                            UNE MARQUE AVEC DES VALEURS
+                        </h1>
+                    </section>
+                    <section>
+                        <div className="row text-white">
+                            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                                <h5 className="text-uppercase">SERVICE CLIENT</h5>
+
+                                <ul className="list-unstyled mb-0">
+                                    <li>
+                                        <a className="text-white" target="_blank" rel="noreferrer" href="https://www.alpinecars.fr/contactez-nous.html">Contacter Alpine</a>
+                                    </li>
+                                    <li>
+                                        <a className="text-white" target="_blank" rel="noreferrer" href="https://www.alpinecars.fr/product-update.html">Campagnes de rappel</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                                <h5 className="text-uppercase">AUTRES SITES</h5>
+
+                                <ul className="list-unstyled mb-0">
+                                    <li>
+                                        <a className="text-white" target="_blank" rel="noreferrer" href="https://www.alpineelfcupseries.com/">Alpine Elf Cup Series</a>
+                                    </li>
+                                    <li>
+                                        <a className="text-white" target="_blank" rel="noreferrer" href="https://boutique.alpinecars.com/">Boutique</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                                <h5 className="text-uppercase">LIENS DIRECTS</h5>
+
+                                <ul className="list-unstyled mb-0">
+                                    <li>
+                                        <a className="text-white" target="_blank" rel="noreferrer" href="http://localhost:3001/Configurators">Configurators</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                                <h5 className="text-uppercase">Links</h5>
+
+                                <ul className="list-unstyled mb-0">
+                                    <li>
+                                        <a className="text1" target="_blank" rel="noreferrer" href="https://fr-fr.facebook.com/alpinesportscars/">Facebook</a>
+                                    </li>
+                                    <li>
+                                        <a className="text3" target="_blank" rel="noreferrer" href="https://x.com/AlpineF1Team?s=20&t=HaDDdmh5jRqowXRevmR_aQ">X</a>
+                                    </li>
+                                    <li>
+                                        <a className="text3" target="_blank" rel="noreferrer" href="https://www.instagram.com/alpinecars/?hl=fr">Instagram</a>
+                                    </li>
+                                    <li>
+                                        <a className="text2" target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCzH4Iwlm8kI09wXbgHvFfIg">Youtube</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className=" LogoFooter">
+                                <NavLink to="/"><img src={Logo} alt="Logo" style={{ width: "200px" }} /></NavLink>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </footer>
+        </section>
+    );
+}
+
+export default Footer;
