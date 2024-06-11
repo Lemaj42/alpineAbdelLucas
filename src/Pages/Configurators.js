e71650b984fd9d8527e2b6441732e73d7d0d954f
lemaj42 <abdelmadjidchabane89@gmail.com>
2024-06-11 16:52:01 +0200

    Section Config dans accueil

--- src/Pages/Configurators.js
+++ src/Pages/Configurators.js
@@ -4,32 +4,24 @@ import { Test } from "../features/configuratorSclice";
 import "./CSS/configurator.css"
 import { Button } from "react-bootstrap";
 
+
 function Configurators() {
 
     const dispatch = useDispatch()
     const voiture = useSelector(state => state.CarConfig)
-    
+
     const loadState = () => {
 
         console.log(voiture)
         dispatch(Test())
     }
 
-    
+
     return (
         <>
-        <section id="configuration">
+            <section id="configuration">
 
-            <img
-                src=""
-                class="img-fluid rounded-top"
-                alt=""
-            />
-
-                  
-            <Button onClick={loadState}> click to see </Button>
-        </section>
-
+            </section>
         </>
     )
 };
--- src/features/configuratorSclice.js
+++ src/features/configuratorSclice.js
@@ -1,12 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-
-    car: {
-        "couleur": "bleu",
-        "modele": "Alpine A110",
-        "roue": "roue de con",
-    },
+    cars: [
+        { name: "Pure", price: 54700 },
+        { name: "Legende", price: 58500, legende: true },
+    ],
+    colors: [
+        { name: "Bleu Alpine", price: 1800 },
+        { name: "Noir Profond", price: 840 },
+        { name: "blanc opaque", price: 0 },
+    ],
+    jantes: [
+        { name: "Standard", price: 0 },
+        { name: "Serac", price: 1000 },
+        { name: "Legende", price: 1000, legende: true },
+    ],
+    scelleries: [
+        { name: "Sièges baquets en cuir noir et Dinamica", legende: false },
+        { name: "Sièges Confort en cuir noir perforé", legende: false },
+        { name: "Sièges Confort en cuir noir", legende: true },
+        { name: "Sièges Confort en cuir brun", legende: true },
+    ],
 };
 
 export const configSlice = createSlice({
