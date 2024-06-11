e6c22909efeab80c7082b77ed570c2fc5018e69f
lucas dechavanne <lucasdechavanne22@gmail.com>
2024-06-11 15:39:37 +0200

    les slices fonionne c bon

--- src/features/configuratorSclice.js
+++ src/features/configuratorSclice.js
@@ -1,24 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const inititalState = {
+const initialState = {
 
     car: {
         "couleur": "bleu",
         "modele": "Alpine A110",
         "roue": "roue de con",
-    }
-} 
+    },
+};
 
 export const configSlice = createSlice({
-    name: "config",
-    inititalState,
+    name: "CarConfig",
+    initialState,
     reducers: {
 
-        Test: (state, action) => {
+        Test: (state) => {
+            let newState = { ...state }
             console.log("ca fonctione")
+            return newState
+        },
+
+        ChangeWheels: (state, action) => {
+            let newState = {
+                ...state,
+                car: {
+                    ...state.car,
+                    roue: action.payload.truc
+                }
+            }
+
+
+            return newState
         }
     }
-}) 
+})
 
 export const { Test } = configSlice.actions
 
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import { store } from './store/store';
 import { Provider } from 'react-redux';
 
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
